bdfd43dc98dbe65c5ab9db4923a091fe3a59a09e
Botond Dénes <bdenes@scylladb.com>
2019-06-19 16:57:19 +0300

    sstable-statistics.py: move code to top-level scope
    As the script is not intended to be imported anymore, there is no point
    in having a `main()` anymore.

--- sstable-statistics.py
+++ sstable-statistics.py
@@ -5,20 +5,15 @@ import json
 import sstable_tools.statistics
 
 
-def main():
-    cmdline_parser = argparse.ArgumentParser()
-    cmdline_parser.add_argument('stats_file', help='stats file to parse')
-    cmdline_parser.add_argument('-f', '--format', choices=['ka', 'la', 'mc'], default='mc', help='sstable format')
+cmdline_parser = argparse.ArgumentParser()
+cmdline_parser.add_argument('stats_file', help='stats file to parse')
+cmdline_parser.add_argument('-f', '--format', choices=['ka', 'la', 'mc'], default='mc', help='sstable format')
 
-    args = cmdline_parser.parse_args()
+args = cmdline_parser.parse_args()
 
-    with open(args.stats_file, 'rb') as f:
-        data = f.read()
+with open(args.stats_file, 'rb') as f:
+    data = f.read()
 
-    metadata = sstable_tools.statistics.parse(data, args.format)
+metadata = sstable_tools.statistics.parse(data, args.format)
 
-    print(json.dumps(metadata, indent=4))
-
-
-if __name__ == '__main__':
-    main()
+print(json.dumps(metadata, indent=4))
